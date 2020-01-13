6be597675fbe8e3061864c00a05f91f52f8e1045
Christopher DiStasio <cadistasio@gmail.com>
2020-01-13 09:45:45 -0600

    Removes unneeded code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-
+// import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
