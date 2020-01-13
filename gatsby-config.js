e7d0d733def2e325fb57707c4d1adfd5f57736b7
Christopher DiStasio <cadistasio@gmail.com>
2020-01-13 09:51:01 -0600

    Removes unneeded code

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,12 +23,8 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `minimal-ui`      
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
