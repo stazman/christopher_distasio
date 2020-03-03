6a936f331ee5d86fc73a47531fc1a8022b2363b8
Christopher DiStasio <cadistasio@gmail.com>
2020-03-03 07:59:11 -0600

    Updates resume link

--- src/components/main.js
+++ src/components/main.js
@@ -139,7 +139,7 @@ const MediaQueries = styled.div`
                     }}
                   >
                     <a
-                      href='https://tinyurl.com/tyra6jz'
+                      href='https://tinyurl.com/rahfnao'
                       style={{
                         textDecoration: 'none',
                         paddingTop: '4rem',
