b86b4747b757c11e0ff6b89a4e072f2d1fa914a1
Christopher DiStasio <cadistasio@gmail.com>
2020-12-14 11:58:44 -0600

    Adds, updates with photo in acceptable format

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/better-profile-pic.HEIC`      },
+        icon: `src/images/new-best-pro-photo.jpg`      },
     },
   ],
 }
