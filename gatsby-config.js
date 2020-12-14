1ed1320c1a880914831d5cb06f67b39bad4cd37d
Christopher DiStasio <cadistasio@gmail.com>
2020-12-14 11:42:29 -0600

    Addresses missing icon error

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`      
-      },
+        display: `minimal-ui`,
+        icon: ``      },
     },
   ],
 }
